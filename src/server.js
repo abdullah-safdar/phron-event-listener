b8330702a85209778b36087b13c557f3d61d0ae5
Abdullah-plutus <115459978+Abdullah-plutus@users.noreply.github.com>
2024-08-30 14:11:07 +0500

    fix

--- src/server.js
+++ src/server.js
@@ -4,43 +4,55 @@ import { sections } from "./constants/index.js";
 const provider = new WsProvider(websocket_url);
 
 async function main() {
-  // Create our API with a default connection to the local node
-  const api = await ApiPromise.create({ provider });
+  try {
+    // Create our API with a default connection to the local node
+    const api = await ApiPromise.create({ provider });
 
-  // Subscribe to system events via storage
-  api.query.system.events((events) => {
-    console.log(`\nReceived ${events.length} events:`);
+    // Subscribe to system events via storage
+    api.query.system.events((events) => {
+      console.log(`\nReceived ${events.length} events:`);
 
-    // Loop through the Vec<EventRecord>
-    events.forEach(async (record) => {
-      // Extract the phase, event and the event types
-      const { event, phase } = record;
-      const types = event.typeDef;
+      // Loop through the Vec<EventRecord>
+      events.forEach(async (record) => {
+        // Extract the phase, event and the event types
+        const { event, phase } = record;
+        const types = event.typeDef;
 
-      // console.log(event);
+        // console.log(event);
 
-      //   {
-      //     method: 'RequiresDecision',
-      //     section: 'committeeManagement',
-      //     index: '0x0e05',
-      //     data: { forSession: '557' }
-      //   }
+        //   {
+        //     method: 'RequiresDecision',
+        //     section: 'committeeManagement',
+        //     index: '0x0e05',
+        //     data: { forSession: '557' }
+        //   }
 
-      // console.log(event.toHuman());
-      //console.log(event);
-      if (sections[event.section]) {
-        if (event.method === "RequiresDecision") {
-          const resp = await fetch(" http://142.132.144.174:5000/AI_extrinsic");
-          const result = await resp.json();
-          if (result === 1) {
-            console.log("The require decision has been passed");
-          } else {
-            console.log("The require decision has been FAILED!!!");
+        // console.log(event.toHuman());
+        //console.log(event);
+        if (sections[event.section]) {
+          if (event.method === "RequiresDecision") {
+            console.log("RequiresDecision");
+            try {
+              const resp = await fetch(
+                "http://142.132.144.174:5000/AI_extrinsic"
+              );
+              const result = await resp.json();
+              console.log("result", result);
+              if (result === 1) {
+                console.log("The require decision has been passed");
+              } else {
+                console.log("The require decision has been FAILED!!!");
+              }
+            } catch (error) {
+              console.log("error", error);
+            }
           }
         }
-      }
+      });
     });
-  });
+  } catch (error) {
+    console.log("error", error);
+  }
 }
 
 // {
@@ -50,7 +62,8 @@ async function main() {
 //   data: [ { reserved: [Object], nonReserved: [Object] } ]
 // }
 
-main().catch((error) => {
-  console.error(error);
-  process.exit(-1);
-});
+main();
+// .catch((error) => {
+//   console.error(error);
+//   process.exit(-1);
+// });
